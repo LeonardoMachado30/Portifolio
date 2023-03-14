cd520c4a2e9a05c9fb580645bfc790bcfb014766
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-03-14 14:25:53 -0300

    CHANGE NEXT CONFIG

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('dotenv').config()} */
+
+const isProd =
+  process.env.NODE_ENV === "production"
+    ? "https://leonardomachado30.github.io/React__app__portifolio/"
+    : process.env.STATIC_ASSETS_URL;
+
+console.log(process.env.STATIC_ASSETS_URL);
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     unoptimized: true,
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+    };
   },
 };
 
