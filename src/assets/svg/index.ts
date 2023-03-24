3a6d4649949aea24423a9818e94aacdb84cc5255
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-03-24 13:37:15 -0300

    ADD Icons

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -12,6 +12,8 @@ import sass from "./sass.svg";
 import php from "./php.svg";
 import gsap from "./gsap.svg";
 import info from "./info.svg";
+import icon_github from "./icon_github.svg";
+import icon_redirect from "./icon_redirect.svg";
 
 
-export { css, gulp, html, javascript, typescript, next, bootstrap, jquery, react, csharp, sass, php, gsap, info }
+export { css, gulp, html, javascript, typescript, next, bootstrap, jquery, react, csharp, sass, php, gsap, info, icon_github, icon_redirect }
