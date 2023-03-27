425810f5fd969bbe7a3249fc532bafc5046f8704
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-03-27 18:38:35 -0300

    Update export.tsx

--- src/components/export.tsx
+++ src/components/export.tsx
@@ -1,25 +1,25 @@
 import Header from "./Header/Header";
-import Content from "./Content/Content";
 import AboutMe from "./AboutMe/AboutMe";
 import Skills from "./Skills/Skills";
 import Schooling from "./Schooling/Schooling";
 import Repositories from "./Repositories/Repositories";
 import Character from "./Character/Character";
-import BlindsEffect from "./BlindsEffect/BlindsEffect";
+import Welcome from "./Welcome/Welcome";
 import StampLinkedIn from "./StampLinkedIn/StampLinkedIn";
 import ContactMe from "./ContactMe/ContactMe";
 import Hexagonal from "./Hexagonal/Hexagonal";
+import Setting from "./Setting/Setting";
 
 export {
   Header,
-  Content,
   AboutMe,
   Skills,
   Schooling,
   Repositories,
   Character,
-  BlindsEffect,
+  Welcome,
   StampLinkedIn,
   ContactMe,
   Hexagonal,
+  Setting,
 };
