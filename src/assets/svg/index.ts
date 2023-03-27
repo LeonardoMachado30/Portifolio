cc0d22b3cadbfb2d190177b3472cf00526c2536f
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-03-27 18:38:24 -0300

    Add: Icons svg

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -14,6 +14,18 @@ import gsap from "./gsap.svg";
 import info from "./info.svg";
 import icon_github from "./icon_github.svg";
 import icon_redirect from "./icon_redirect.svg";
+import icon_whatsapp from "./icon_whatsapp.svg";
+import icon_email from "./icon_email.svg";
+import icon_settings from "./icon_settings.svg";
+import icon_Bandeiro_Brasil from "./icon_Bandeiro_Brasil.svg";
+import icons_arrow_down from "./icons_arrow_down.svg";
+import icons_arrow_up from "./icons_arrow_up.svg";
+import icon_Bandeira_EUA from "./icon_Bandeira_EUA.svg";
+import icon_cube from "./icon_cube.svg";
 
-
-export { css, gulp, html, javascript, typescript, next, bootstrap, jquery, react, csharp, sass, php, gsap, info, icon_github, icon_redirect }
+export {
+    css, gulp, html, javascript, typescript, next, bootstrap,
+    jquery, react, csharp, sass, php, gsap, info,
+    icon_github, icon_redirect, icon_whatsapp, icon_email, icon_settings, icon_Bandeiro_Brasil,
+    icons_arrow_down, icons_arrow_up, icon_Bandeira_EUA, icon_cube
+}
