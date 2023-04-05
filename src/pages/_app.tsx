8fd02c2c5a71c6d03e064c692acf2f0e826e571f
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-04-05 12:29:21 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import ReactGA from "react-ga";
 const TRACKING_ID = "G-JW0FSYRYGZ"; // YOUR_OWN_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 import type { AppProps } from "next/app";
 import "@/styles/globals.scss";
 
