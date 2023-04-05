409c7699f3b20e3a3f31b031e3e8b26c7ad3ffef
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-04-05 13:00:30 -0300

    ADD: Analitcs
    ADD: Analitcs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,28 @@
-import ReactGA from "react-ga";
-const TRACKING_ID = "G-JW0FSYRYGZ"; // YOUR_OWN_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
-ReactGA.pageview(window.location.pathname + window.location.search);
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import "@/styles/globals.scss";
 
+import * as gtag from "@/utils/Analitcs/gtag";
+import Analytics from "@/utils/Analitcs/Analitcs";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,7 @@
+import ReactGA from "react-ga";
+const TRACKING_ID = "G-JW0FSYRYGZ"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+// ReactGA.pageview(window.location.pathname + window.location.search);
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
