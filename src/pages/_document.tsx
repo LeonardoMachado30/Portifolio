ff1666e04001ea8946c77c523fddc48ca3af7e0a
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-04-05 13:09:29 -0300

    ADD type gtag
    ADD type gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,3 @@
-import ReactGA from "react-ga";
-const TRACKING_ID = "G-JW0FSYRYGZ"; // YOUR_OWN_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
-// ReactGA.pageview(window.location.pathname + window.location.search);
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
--- src/utils/Analitcs/gtag.ts
+++ src/utils/Analitcs/gtag.ts
@@ -1,5 +1,9 @@
+declare global {
+  interface Window {
+    gtag: any;
+  }
+}
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
-
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
   window.gtag("config", GA_TRACKING_ID, {
