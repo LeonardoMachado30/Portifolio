91680199cb9223222addd589a9bb4c8252864992
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-04-05 12:24:48 -0300

    ADD: google analytics
    ADD: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import ReactGA from "react-ga";
+const TRACKING_ID = "G-JW0FSYRYGZ"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
