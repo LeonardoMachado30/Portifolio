e1294a77d5c5f2dc04e5040994db8d010a332482
flmpl <flmp.leonardo@gmail.com>
2025-04-11 16:23:09 -0300

    ---

--- src/components/AboutMe/ressourcePT.ts
+++ src/components/AboutMe/ressourcePT.ts
@@ -1,29 +1,8 @@
 export const localizer = {
   title: `SOBRE MIM`,
-  p1: `Olá, me chamo Flávio, iniciei minha jornada na tecnologia trabalhando com reparos em
-      dispositivos eletrônicos, como notebooks, computadores, celulares e
-      outros.Realizei um estágio no Colégio Objetivo por 6 meses como suporte
-      técnico, ajudando os funcionários durante a pandemia com as aulas online.
-      Minhas responsabilidades incluíam suporte aos aplicativos do pacote
-      Office, configurações das aulas em plataformas como Meet, Zoom e Teams,
-    bem como a formatação e manutenção dos equipamentos da escola.Após a
-      experiência como suporte, comecei a trabalhar como programador no site
-      "Trabalha Brasil" como desenvolvedor front- end.Aprendi sobre a criação de
-      landing pages, telas dinâmicas, transferência de dados, consumo de APIs,
-    otimização de navegação e SEO.`,
-  p2: `Minha meta profissional é me tornar um desenvolvedor de jogos, usando meus
-      conhecimentos e habilidades para criar soluções inovadoras e proporcionar
-      experiências incríveis aos jogadores. Para alcançar este objetivo, estou
-      sempre buscando novos desafios e oportunidades de aprendizado na área de
-      desenvolvimento de jogos. Acredito que minhas habilidades em programação,
-      criação de landing pages e otimização de navegação serão valiosas na
-      criação de jogos dinâmicos e interativos. Além disso, meu conhecimento em
-      tecnologias como React e NextJs, bem como meu trabalho anterior como
-      suporte técnico, me permitirão ajudar a resolver problemas de maneira
-      eficiente e garantir a qualidade final do produto.`,
-  p3: `Estou animado com a possibilidade de usar minhas habilidades para
-      contribuir com a criação de jogos que possam ser apreciados por pessoas de
-      todas as idades e lugares. Acredito que a combinação de minhas habilidades
-      técnicas e minha paixão por jogos tornará possível alcançar meu objetivo
-      de se tornar um desenvolvedor de jogos de sucesso.`,
+  p1: `Com formação em Análise e Desenvolvimento de Sistemas pela Universidade Paulista, destaco-me pelo profundo conhecimento técnico e pela capacidade de implementar soluções inovadoras em ambientes digitais. No Grupo LAPM, liderança técnica na transição de Vue 2 para Vue 3 resultou em aumento de 60% na performance das aplicações, otimizando a manutenção e reforçando a escalabilidade.
+
+Minha expertise em biometria facial transformou a coleta de dados biométricos, elevando a eficiência em 80% e reduzindo custos operacionais. Busco continuamente a excelência no desenvolvimento de projetos, colaborando estreitamente com equipes multidisciplinares para entregar produtos que superem as expectativas dos usuários e tragam resultados significativos para a empresa.`,
+  p2: ``,
+  p3: ``,
 };
